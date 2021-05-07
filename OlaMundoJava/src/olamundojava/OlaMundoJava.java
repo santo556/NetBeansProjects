be3557eeaceb0506f80b98dc3ebbde133c718f3c
santo556 <ve727402gafanhoto@gmail.com>
2021-05-06 23:18:53 -0300

    PrimeiroOlaMundoJava

---
+++ OlaMundoJava/src/olamundojava/OlaMundoJava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package olamundojava;
+
+/**
+ *
+ * @author y17ad
+ */
+public class OlaMundoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Olá, Mundo!");
+        System.out.println("Estou aprendendo Java.");
+        System.out.println("Todas as pessoas do planeta devriam aprender a programar!");
+    }
+    
+}
